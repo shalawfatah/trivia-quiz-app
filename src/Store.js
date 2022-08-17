a9aad95a51dc2bb73a602696a1c0072379da13d4
shalawkarim <shalaw@glaciermediadigital.ca>
2022-08-16 20:47:08 -0700

    quiz finished

--- src/Store.js
+++ src/Store.js
@@ -3,3 +3,5 @@ import { atom } from "jotai";
 // SHARED DATA IS STORED HERE TO BE ACCESSED BY ALL COMPONENTS
 export const dataAtom = atom([])
 export const questionNumberAtom = atom(0)
+export const markAtom = atom(0)
+export const resultAtom = atom(false)
--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,16 +1,36 @@
+import { useAtom } from 'jotai'
 import React from 'react'
+import { dataAtom, markAtom, questionNumberAtom, resultAtom } from '../Store'
+import { Navigate } from 'react-router-dom'
 
 const Answer = () => {
+  const [data, setData] = useAtom(dataAtom)
+  const [questionNumber, setQuestionNumber] = useAtom(questionNumberAtom)
+  const [mark, setMark] = useAtom(markAtom)
+  const [result, setResult] = useAtom(resultAtom)
+
   const handleTrue = () => {
-
+    data[questionNumber]?.correct_answer == "True" && setMark(prev => prev + 1)
+    questionNumber < 9 && setQuestionNumber(prev => prev + 1)
+    if(questionNumber == 9) {
+      data[questionNumber]?.correct_answer == "True" && setMark(prev => prev + 1)
+      setResult(true)
+    }
   }
-  const handleFalse = () => {
 
+  const handleFalse = () => {
+    data[questionNumber]?.correct_answer == "False" && setMark(prev => prev + 1)
+    questionNumber < 9 && setQuestionNumber(prev => prev + 1)
+    if(questionNumber == 9) {
+      data[questionNumber]?.correct_answer == "False" && setMark(prev => prev + 1)
+      setResult(true)
+    }
   }
   return (
     <div className='btn-container'>
       <button className='btn' onClick={handleTrue}>True</button>
       <button className='btn' onClick={handleFalse}>False</button>
+      {result == true && <Navigate to='/result' />}
     </div>
   )
 }
